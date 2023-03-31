b8588a1a858a8afd918149ca07f7769ff2c6404b
Nikolay <vybornoff@outlook.com>
2023-03-31 17:02:09 +0500

    Created/Reg(admin) model Post. QuerySet from DB -> main Page.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Nikolay',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 30, 2023',
-    },
-    {
-        'author': 'Yana',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 31, 2023',
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all().order_by('date_posted')
     }
     return render(request, 'blog/home.html', context)
 
