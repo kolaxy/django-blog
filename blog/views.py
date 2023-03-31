e204dce067ec539bf3c9688af837a1500e68aef5
Nikolay <vybornoff@outlook.com>
2023-03-31 12:49:40 +0500

    Templates, CSS

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Nikolay',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 30, 2023',
+    },
+    {
+        'author': 'Yana',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 31, 2023',
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
